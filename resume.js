2481a21144922ee70737bee405e4bdd64f75bcd1
Aravinth D <88095852+AudiAravinth@users.noreply.github.com>
2021-08-23 13:25:11 +0530

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,26 @@
+var obj = {Name:"Aravinth",
+           DOB:"26-10-1999",
+           Blood_Group:"O+ve",
+           Address:"Sathyamangalam",
+           Phone:9790491175,
+           Email:"aravinthdandapani26@gmail.com",
+           Career_Objective:"Seeking for a career which provides me an opportunity to improve my skills and helps me to build myself.",
+           Educational_Qualification:["Completed SSLC at Sri Ragavendra Matriculation with 92.6% (2015)","Completed HSC at Sri Ragavendra Matriculation School with 69.6% (2017)","Completed BE. Mechanical Engineering at K.S.R. College of Engineering with 8.57 CGPA (2021)"],
+           Area_Of_Intrest:["Designing","Full Stack Developing","Automation"], 
+           Hobbies:["Cycling","Driving","Cooking","Listening Songs"],
+           Languages_Known:["English","Tamil","Telugu"],
+           Projects:"Fabricated the regular class airmodel and participated in the national level event “ 5th – SAEINDIA SOUTHERN SECTION AERO DESIGN CHALLENGE – 2020”",
+           Co_Curricular_Activities:["Presented a paper on “ DRIVE BY WIRE IN AUTOMOBILE SYSTEM”  at KSRCE","Workshop on “DATA ANALYTICS USING PYTHON” at Kongu Engineering College.","International Workshop on “ARDUINO IN ROBOTICS – IWAR’18”  at Coimbatore Institute of Technology ","in CISCO NETWORKING ACADEMY on “ENTREPRENEURSHIP COURSE”","Google Digital Unlocked on “ THE FUNDAMENTALS OF DIGITAL MARKETING”","GUVI – Step into Robotic Process Automation","Automation Anywhere RPA – Bot Aspirant Batch.","SPORTS – Road Cycling, Mini Marathon, KHO-KHO.","National Cadet Corps"],
+           InternshipandTraining_Program:["Roots Industries India Limited during 10.03.2020 to 20.03.2020 ","Latest technical features & systems available in BMW 5 SERIES sedan car at GOODWIN MOTORS for the period of 05.12.2018 to 21.12.2018 ","Hands on experience with Product Design and Development (CREO) at KSR Centre Of Excellence for Manufacturing. ( July 2019 To December 2019)."],
+
+         };
+         for(var store in obj){
+         console.log(obj[store]);
+         }
+           
+
+
+
+
+
+    
